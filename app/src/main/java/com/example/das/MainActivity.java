031847a0cee2fa9e1c50cf1e8b7c5dc1a13d647a
Igor <igor.game999.ig@gmail.com>
2021-05-11 01:27:49 +0200

    Mapa empezado, pero no funciona

--- app/src/main/java/com/example/das/MainActivity.java
+++ app/src/main/java/com/example/das/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.das;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.Observer;
 import androidx.work.Data;
 import androidx.work.ExistingWorkPolicy;
@@ -8,19 +10,56 @@ import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TabHost;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.concurrent.Executor;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private FirebaseAuth firebaseAuth;
     private String id;
+
+
+    FusedLocationProviderClient proveedordelocalizacion;
+    LocationCallback actualizador;
+    Marker jugador;
+    Boolean seguir = true;
+    ArrayList<Marker> listaMarkers = new ArrayList<>(5);
+    ArrayList<GroundOverlay> listaCirculos = new ArrayList<>(5);
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -87,6 +126,15 @@ public class MainActivity extends AppCompatActivity {
         spec.setIndicator("Perfil");
         tabhost.addTab(spec);
 
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 24);
+        } else {
+            SupportMapFragment elfragmento = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentoMapa);
+            elfragmento.getMapAsync(this);
+        }
+
+
     }
 
     //Comprueba si el usuario ha sido registrado
@@ -122,6 +170,152 @@ public class MainActivity extends AppCompatActivity {
         WorkManager.getInstance(getApplication().getBaseContext()).enqueueUniqueWork("existeUsuario", ExistingWorkPolicy.REPLACE, requesContrasena);
 
 
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //Si no tiene permisos, vuelve a la ventana principal
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Intent i = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }
+        //Se crea el proveedor de localización
+        proveedordelocalizacion =
+                LocationServices.getFusedLocationProviderClient(this);
+        proveedordelocalizacion.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    //Si es exitoso
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            //Se almacenan las coordenadas actuales
+                            LatLng nuevascoordenadas = new LatLng(location.getLatitude(), location.getLongitude());
+
+                            //Se le pone el estilo al mapa para que parezca Marte
+                            //Web para mapas editados: https://mapstyle.withgoogle.com/
+                            googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MainActivity.this, R.raw.estilo_mapa));
+
+                            //Se le asignan varios listeners para decidir si la cámara tiene que seguir al jugador o no, de modo que
+                            //cuando se pulsa el botón para centrar la cámara siga al jugador, pero tras interactuar con el mapa, deje de seguir al jugador.
+                            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                                @Override
+                                public void onMapClick(LatLng latLng) {
+                                    seguir = false;
+                                }
+                            });
+
+                            googleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+                                @Override
+                                public void onMapLongClick(LatLng latLng) {
+                                    seguir = false;
+                                }
+                            });
+
+                            googleMap.setOnCameraMoveStartedListener(new GoogleMap.OnCameraMoveStartedListener() {
+                                @Override
+                                public void onCameraMoveStarted(int reason) {
+                                    if (reason == GoogleMap.OnCameraMoveStartedListener.REASON_GESTURE) {
+                                        seguir = false;
+                                    }
+                                }
+                            });
+
+                            //Se establece un listener para que no salgan etiquetas al pulsar en los marcadores
+                            googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                                @Override
+                                public boolean onMarkerClick(Marker marker) {
+                                    return true;
+                                }
+                            });
+
+                            //Se restringe el movimiento del usuario en el mapa, para que no pueda hacer demasiado zoom-out, y no pueda cambiar el tilt.
+                            googleMap.getUiSettings().setTiltGesturesEnabled(false);
+                            googleMap.setMinZoomPreference(17);
+                            googleMap.setMaxZoomPreference(19);
+                            googleMap.getUiSettings().setScrollGesturesEnabledDuringRotateOrZoom(false);
+                            googleMap.getUiSettings().setZoomControlsEnabled(true);
+                            CameraPosition Poscam = new CameraPosition.Builder()
+                                    .target(nuevascoordenadas)
+                                    .zoom(18)
+                                    .tilt(80)
+                                    .build();
+                            CameraUpdate otravista = CameraUpdateFactory.newCameraPosition(Poscam);
+
+                            //Se establece el botón de centrar, que al pulsarlo mueve la cámara a la localización actual del jugador.
+                            /*ImageView botonCentrar = findViewById(R.id.botonCentrar);
+                            botonCentrar.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    LocationServices.getFusedLocationProviderClient(MainActivity.this);
+                                    seguir = true;
+                                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MapaActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                                        return;
+                                    }
+                                    proveedordelocalizacion.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                                        @Override
+                                        public void onSuccess(Location location) {
+                                            LatLng pos = new LatLng(location.getLatitude(), location.getLongitude());
+                                            CameraUpdate actualizar = CameraUpdateFactory.newLatLng(pos);
+                                            googleMap.animateCamera(actualizar);
+                                        }
+                                    });
+                                }
+                            });*/
+
+                            //Se añade en el mapa un marcador en la ubicación actual del jugador, para representarlo.
+                            jugador = googleMap.addMarker(new MarkerOptions()
+                                    .position(nuevascoordenadas)
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.marcador))
+                                    .title("Yo"));
+
+                            //Se inicializan los marcadores de brote en posiciones aleatorias
+                            //iniciarMarcadores(googleMap);
+                            //actualizarMarcadores(googleMap);
+
+                            googleMap.moveCamera(otravista);
+                        } else {
+                            return;
+                        }
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    //Si falla no se hace nada
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+        //Se establece la actualización periódica de la posición del jugador y de los marcadores de brote.
+        actualizador = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                if (locationResult != null) {
+                    LatLng pos = new LatLng(locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude());
+                    CameraUpdate actualizar = CameraUpdateFactory.newLatLng(pos);
+                    if (seguir){googleMap.animateCamera(actualizar);}
+                    //Se elimina el antiguo marcador y se pone uno nuevo.
+                    jugador.remove();
+                    jugador = googleMap.addMarker(new MarkerOptions()
+                            .position(pos)
+                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.marcador)));
+                    //Se actualizan los marcadores de brote.
+                    //actualizarMarcadores(googleMap);
+                } else {
+                    return;
+                }
+            }
+        };
+
+        LocationRequest peticion = LocationRequest.create();
+        peticion.setInterval(3000);
+        peticion.setFastestInterval(1000);
+        peticion.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        proveedordelocalizacion.requestLocationUpdates(peticion, actualizador, null);
 
     }
 }
--- app/src/main/java/com/example/das/MapaFragment.java
+++ app/src/main/java/com/example/das/MapaFragment.java
@@ -1,57 +1,75 @@
 package com.example.das;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.ArrayList;
+import java.util.concurrent.Executor;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link MapaFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MapaFragment extends Fragment {
+public class MapaFragment extends SupportMapFragment implements OnMapReadyCallback {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FusedLocationProviderClient proveedordelocalizacion;
+    LocationCallback actualizador;
+    Marker jugador;
+    Boolean seguir = true;
+    ArrayList<Marker> listaMarkers = new ArrayList<>(5);
+    ArrayList<GroundOverlay> listaCirculos = new ArrayList<>(5);
 
     public MapaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MapaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MapaFragment newInstance(String param1, String param2) {
         MapaFragment fragment = new MapaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 24);
+        } else {
+            //SupportMapFragment elfragmento = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.fragmentoMapa);
+            this.getMapAsync(this);
         }
     }
 
@@ -60,5 +78,117 @@ public class MapaFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mapa, container, false);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //Si no tiene permisos, vuelve a la ventana principal
+        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+        }
+        //Se crea el proveedor de localización
+        proveedordelocalizacion =
+                LocationServices.getFusedLocationProviderClient(this.getActivity());
+        proveedordelocalizacion.getLastLocation()
+                .addOnSuccessListener((Executor) this, new OnSuccessListener<Location>() {
+                    @Override
+                    //Si es exitoso
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            //Se almacenan las coordenadas actuales
+                            LatLng nuevascoordenadas = new LatLng(location.getLatitude(), location.getLongitude());
+
+                            //Se le pone el estilo al mapa para que parezca Marte
+                            //Web para mapas editados: https://mapstyle.withgoogle.com/
+                            googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MapaFragment.this.getContext(), R.raw.estilo_mapa));
+
+                            //Se le asignan varios listeners para decidir si la cámara tiene que seguir al jugador o no, de modo que
+                            //cuando se pulsa el botón para centrar la cámara siga al jugador, pero tras interactuar con el mapa, deje de seguir al jugador.
+                            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                                @Override
+                                public void onMapClick(LatLng latLng) {
+                                    seguir = false;
+                                }
+                            });
+
+                            googleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+                                @Override
+                                public void onMapLongClick(LatLng latLng) {
+                                    seguir = false;
+                                }
+                            });
+
+                            googleMap.setOnCameraMoveStartedListener(new GoogleMap.OnCameraMoveStartedListener() {
+                                @Override
+                                public void onCameraMoveStarted(int reason) {
+                                    if (reason == GoogleMap.OnCameraMoveStartedListener.REASON_GESTURE) {
+                                        seguir = false;
+                                    }
+                                }
+                            });
+
+                            //Se establece un listener para que no salgan etiquetas al pulsar en los marcadores
+                            googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                                @Override
+                                public boolean onMarkerClick(Marker marker) {
+                                    return true;
+                                }
+                            });
+
+                            //Se restringe el movimiento del usuario en el mapa, para que no pueda hacer demasiado zoom-out, y no pueda cambiar el tilt.
+                            googleMap.getUiSettings().setTiltGesturesEnabled(false);
+                            googleMap.setMinZoomPreference(17);
+                            googleMap.setMaxZoomPreference(19);
+                            googleMap.getUiSettings().setScrollGesturesEnabledDuringRotateOrZoom(false);
+                            googleMap.getUiSettings().setZoomControlsEnabled(true);
+                            CameraPosition Poscam = new CameraPosition.Builder()
+                                    .target(nuevascoordenadas)
+                                    .zoom(18)
+                                    .tilt(80)
+                                    .build();
+                            CameraUpdate otravista = CameraUpdateFactory.newCameraPosition(Poscam);
+
+                            //Se establece el botón de centrar, que al pulsarlo mueve la cámara a la localización actual del jugador.
+
+                            //Se añade en el mapa un marcador en la ubicación actual del jugador, para representarlo.
+                            jugador = googleMap.addMarker(new MarkerOptions()
+                                    .position(nuevascoordenadas)
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.marcador))
+                                    .title("Yo"));
+
+                            googleMap.moveCamera(otravista);
+                        } else {
+                            return;
+                        }
+                    }
+                });
+
+        //Se establece la actualización periódica de la posición del jugador y de los marcadores de brote.
+        actualizador = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                if (locationResult != null) {
+                    LatLng pos = new LatLng(locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude());
+                    CameraUpdate actualizar = CameraUpdateFactory.newLatLng(pos);
+                    if (seguir){googleMap.animateCamera(actualizar);}
+                    //Se elimina el antiguo marcador y se pone uno nuevo.
+                    jugador.remove();
+                    jugador = googleMap.addMarker(new MarkerOptions()
+                            .position(pos)
+                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.marcador)));
+                } else {
+                    return;
+                }
+            }
+        };
+
+        LocationRequest peticion = LocationRequest.create();
+        peticion.setInterval(3000);
+        peticion.setFastestInterval(1000);
+        peticion.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        proveedordelocalizacion.requestLocationUpdates(peticion, actualizador, null);
+
     }
 }
